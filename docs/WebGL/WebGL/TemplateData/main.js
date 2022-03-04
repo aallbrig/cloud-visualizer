08be9ffdb7ba6773c73b961d67bb83f7a8b670ef
aallbrig <aallbrig@gmail.com>
2022-03-04 21:26:26 +0000

    Deploying to github-pages from @ aallbrig/cloud-visualizer@3d66f188e169ca083496674b3539e97dc1b3cdad 🚀

---
+++ docs/WebGL/WebGL/TemplateData/main.js
@@ -0,0 +1,14 @@
+var canvas = document.querySelector("#unity-canvas");
+
+var config = {
+    dataUrl: "Build/WebGL.data",
+    frameworkUrl: "Build/WebGL.framework.js",
+    codeUrl: "Build/WebGL.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Andrew Allbright",
+    productName: "Cloud Vizualizer Client",
+    productVersion: "0.0.15",
+    devicePixelRatio: 1,
+}
+
+createUnityInstance(canvas, config);
