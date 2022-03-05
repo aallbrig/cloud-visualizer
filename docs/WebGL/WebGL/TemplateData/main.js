97b06454af1228ad41b5cc6e9d0556383bd9e674
aallbrig <aallbrig@users.noreply.github.com>
2022-03-05 19:52:18 +0000

    Deploying to github-pages from @ aallbrig/cloud-visualizer@0596d9ed8acc4718af2b70f7cccb64b7e3a742bf 🚀

--- docs/WebGL/WebGL/TemplateData/main.js
+++ docs/WebGL/WebGL/TemplateData/main.js
@@ -7,7 +7,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "Cloud Vizualizer Client",
-    productVersion: "0.0.17",
+    productVersion: "0.0.18",
     devicePixelRatio: 1,
 }
 
